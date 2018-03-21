6ef47eb826023ccdba732a032cf22839d3ddd8b3
chiragpaliwal <chirag.paliwal06@gmail.com>
2018-03-21 23:00:29 +0530

    loading options from json

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,30 @@ anc.Initialize = function() {
         console.log("query done");
     })
 }
+
+// json to populate solutions datalist
+let id = 0;
+// Get the <datalist> and <input> elements.
+let dataList = document.getElementById('json-datalist');
+let input = document.getElementById('ajax');
+
+let jsonOptions = [
+    {'id': id, 'data': 'Lemon'},
+    {'id': ++id, 'data': 'Lemon1'},
+    {'id': ++id, 'data': 'Lemon2'},
+    {'id': ++id, 'data': 'Lemon3'},
+    {'id': ++id, 'data': 'Lemon4'}
+]
+
+jsonOptions.forEach((item) => {
+    // Create a new <option> element.
+    let option = document.createElement('option');
+    option.value = item.data;
+
+    // Add the <option> element to the <datalist>.
+    dataList.appendChild(option);
+
+});
+
+// Update the placeholder text.
+input.placeholder = 'Select a Solution';
