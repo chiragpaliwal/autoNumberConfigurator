22eb9cdc3bc36442d0c3fc44bf88d91ea3678658
Chirag Paliwal <chirag.paliwal06@gmail.com>
2018-03-24 16:12:54 +0530

    retrieved json to display solutions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,31 @@
-var anc = window.anc || {};
+let Anc = window.Anc || {};
 
-anc.Initialize = function() {
+Anc.Initialize = function() {
     // Load solutions
-    var solutionsUri = "/solutions?$select=friendlyname,version&$expand=publisherid($select=customizationprefix)&$filter=ismanaged eq false and  solutiontype eq 0"
+    
+    let solutionsUri = "/solutions?$select=friendlyname,version&$expand=publisherid($select=customizationprefix)&$filter=ismanaged eq false and  solutiontype eq 0";
     Sdk.request("GET", clientUrl + webAPIPath + solutionsUri, null, true)
     .then(function (req) {
-        console.log("query done");
+    debugger;
+    // Get the <datalist> and <input> elements.
+    let dataList = document.getElementById('json-datalist');
+    let input = document.getElementById('ajax');
+    input.placeholder = 'Select a Solution';
+    let jsonOptions = JSON.parse(req.response);
+    if(jsonOptions != undefined || jsonOptions != null) {
+        jsonOptions.value.forEach((item) => {
+            // Create a new <option> element.
+            let option = document.createElement('option');
+            option.id = item.solutionid;
+            option.value = item.friendlyname + " (" + item.version + ")";
+            // Add the <option> element to the <datalist>.
+            dataList.appendChild(option);
+        });
+    }
+    console.log("query done");
     })
 }
-
-// json to populate solutions datalist
-let id = 0;
-// Get the <datalist> and <input> elements.
-let dataList = document.getElementById('json-datalist');
-let input = document.getElementById('ajax');
-
-let jsonOptions = [
-    {'id': id, 'data': 'Lemon'},
-    {'id': ++id, 'data': 'Apple'},
-    {'id': ++id, 'data': 'Watermelon'},
-    {'id': ++id, 'data': 'Orange'},
-    {'id': ++id, 'data': 'Strawberry'}
-]
-
-jsonOptions.forEach((item) => {
-    // Create a new <option> element.
-    let option = document.createElement('option');
-    option.value = item.data;
-
-    // Add the <option> element to the <datalist>.
-    dataList.appendChild(option);
-
-});
-
-// Update the placeholder text.
-input.placeholder = 'Select a Solution';
-
+window.onload = Anc.Initialize();
 
 let enableNext = () => {
     let disabledInput = document.getElementById('disabledTextInput');
