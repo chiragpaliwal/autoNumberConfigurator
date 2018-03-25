4c04110c6ee07add7ee5f99c57f34327dc898c66
Chirag Paliwal <chirag.paliwal06@gmail.com>
2018-03-25 14:51:01 +0530

    loading entitied

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ let Anc = window.Anc || {};
 
 Anc.Initialize = function() {
     // Load solutions
-    
+
     let solutionsUri = "/solutions?$select=friendlyname,version&$expand=publisherid($select=customizationprefix)&$filter=ismanaged eq false and  solutiontype eq 0";
     Sdk.request("GET", clientUrl + webAPIPath + solutionsUri, null, true)
     .then(function (req) {
@@ -23,6 +23,16 @@ Anc.Initialize = function() {
         });
     }
     console.log("query done");
+    });
+
+    // Load Existing Auto Numbers
+}
+
+Anc.LoadEntities = function () {
+    let entitiesUrl = "/EntityDefinitions?$select=DisplayName,LogicalName&$filter=CanCreateAttributes/Value eq true"
+    Sdk.request("GET", clientUrl + webAPIPath + entitiesUrl, null, true)
+    .then(function () {
+        
     })
 }
 window.onload = Anc.Initialize();
