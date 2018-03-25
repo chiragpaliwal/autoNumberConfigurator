ede5bcf08a6268799485ea440e0664e343541ba1
chiragpaliwal <chirag.paliwal06@gmail.com>
2018-03-25 14:48:06 +0530

    alerts final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,49 @@
 let Anc = window.Anc || {};
 
+MicroModal.init({
+    closeTrigger: 'data-custom-close'
+});
+
 Anc.Initialize = function() {
     // Load solutions
-    
+    MicroModal.show('modal-1');
+
     let solutionsUri = "/solutions?$select=friendlyname,version&$expand=publisherid($select=customizationprefix)&$filter=ismanaged eq false and  solutiontype eq 0";
     Sdk.request("GET", clientUrl + webAPIPath + solutionsUri, null, true)
     .then(function (req) {
     debugger;
     // Get the <datalist> and <input> elements.
-    let dataList = document.getElementById('json-datalist');
-    let input = document.getElementById('ajax');
-    input.placeholder = 'Select a Solution';
-    let jsonOptions = JSON.parse(req.response);
-    if(jsonOptions != undefined || jsonOptions != null) {
-        jsonOptions.value.forEach((item) => {
-            // Create a new <option> element.
+    // let dataList = document.getElementById('json-datalist');
+    // let input = document.getElementById('ajax');
+    // input.placeholder = 'Select a Solution';
+    // let jsonOptions = JSON.parse(req.response);
+    // if(jsonOptions != undefined || jsonOptions != null) {
+    //     jsonOptions.value.forEach((item) => {
+    //         // Create a new <option> element.
+    //         let option = document.createElement('option');
+    //         option.id = item.solutionid;
+    //         option.value = item.friendlyname + " (" + item.version + ")";
+    //         // Add the <option> element to the <datalist>.
+    //         dataList.appendChild(option);
+    //     });
+
+    let select = document.getElementById('solutions-select');
+    let jsonForSolutions = JSON.parse(req.response);
+    if(jsonForSolutions != undefined || jsonForSolutions != null){
+        jsonForSolutions.value.forEach((item) => {
             let option = document.createElement('option');
             option.id = item.solutionid;
-            option.value = item.friendlyname + " (" + item.version + ")";
-            // Add the <option> element to the <datalist>.
-            dataList.appendChild(option);
+            option.innerHTML = item.friendlyname + "(" + item.version + ")";
+            // Add the <option> element to the <select>.
+            select.appendChild(option);
         });
+    }else{
+        $('solutions-icon').toggleClass('fa-sync-alt fa-times').removeClass('fa-spin');
     }
+    
+    $('#solutions-icon').toggleClass('fa-sync-alt fa-check').removeClass('fa-spin');
+    MicroModal.close('modal-1');
+
     console.log("query done");
     })
 }
