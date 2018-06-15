56f39f5113b452eb966f9c280be3b2c15c504b88
Chirag Paliwal <chirag.paliwal06@gmail.com>
2018-06-15 17:27:13 +0530

    Populated existing AN and AN properties

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,15 @@ MicroModal.init({
     closeTrigger: 'data-custom-close'
 });
 
+// Globals
+var table = null;
+
 Anc.Initialize = function() {
     // Load solutions
     MicroModal.show('modal-1');
     let solutionsUri = "/solutions?$select=friendlyname,version&$expand=publisherid($select=customizationprefix)&$filter=ismanaged eq false and  solutiontype eq 0";
     Sdk.request("GET", clientUrl + webAPIPath + solutionsUri, null, true)
     .then(function (req) {
-        debugger;
         let select = document.getElementById('solutions-select');
         let jsonForSolutions = JSON.parse(req.response);
         if(jsonForSolutions != undefined || jsonForSolutions != null){
@@ -68,35 +70,12 @@ Anc.LoadEntities = function () {
     });
 }
 
-Anc.LoadExistingAutoNumbers = function () {
-    let autoNumbersUrl = "/api/data/v9.0/EntityDefinitions(LogicalName='account')/Attributes?$select=DisplayName,AutoNumberFormat,LogicalName&$filter=AttributeType eq Microsoft.Dynamics.CRM.AttributeTypeCode'String' and (AutoNumberFormat ne '' and AutoNumberFormat ne null)";
-    Sdk.request("GET", clientUrl + webAPIPath + autoNumbersUrl, null, true)
-    .then(function (req) {
-        let dataList = document.getElementById('json-datalist');
-        let input = document.getElementById('entity-input');
-        input.placeholder = 'Select an Entity';
-        let jsonForEntity = JSON.parse(req.response);
-        if(jsonForEntity != undefined || jsonForEntity != null) {
-            jsonForEntity.value.forEach((item) => {
-                // Create a new <option> element.
-                let option = document.createElement('option');
-                option.id = item.MetadataId;
-                var displayname = item.DisplayName.UserLocalizedLabel === null ? null : item.DisplayName.UserLocalizedLabel.Label; 
-                if(displayname === null) {
-                    option.value = item.LogicalName;    
-                } else option.value = displayname + " (" + item.LogicalName + ")";
-                // Add the <option> element to the <datalist>.
-                dataList.appendChild(option);
-            });
-        }else{
-            $('entities-icon').toggleClass('fa-sync-alt fa-times').removeClass('fa-spin');
-        }
-        $('#entities-icon').toggleClass('fa-sync-alt fa-check').removeClass('fa-spin');
-        MicroModal.close('modal-1');
-    });
-}
-
 window.onload = Anc.Initialize();
+
+let enableNext = () => {
+    let disabledInput = document.getElementById('disabledTextInput');
+    disabledInput.removeAttribute('disabled');
+}
 
 let checkEnable = () => {
     let disabledInput = document.getElementById('entity-input');
@@ -106,21 +85,103 @@ let checkEnable = () => {
     }
 }
 
-let enableNext = () => {
-    let disabledInput = document.getElementById('disabledTextInput');
-    disabledInput.removeAttribute('disabled');
+Anc.LoadExistingAutoNumbers = function () {
+    let entityInputVal = $('#entity-input').val();
+    let regExp = /\(([^)]+)\)/;
+    let logicalname = regExp.exec(entityInputVal);
+
+    let autoNumbersUrl = "/EntityDefinitions(LogicalName='" + logicalname[1] + "')/Attributes?$select=DisplayName,AutoNumberFormat,LogicalName,Description&$filter=AttributeType eq Microsoft.Dynamics.CRM.AttributeTypeCode'String' and (AutoNumberFormat ne '' and AutoNumberFormat ne null)";
+    Sdk.request("GET", clientUrl + webAPIPath + autoNumbersUrl, null, true)
+    .then(function (req) {
+        let jsonForExistingAN = JSON.parse(req.response);
+        if(jsonForExistingAN != undefined || jsonForExistingAN != null) {
+            let existingANbody = document.getElementById("table-body");
+            if (table != null) table.destroy();
+            table = $('#existingANtable').DataTable({
+                data : jsonForExistingAN.value,
+                "bFilter": false,
+                "paging": false,
+                "ordering": false,
+                select: true,
+                "columnDefs": [
+                    {
+                        "targets": [3],
+                        "visible": false
+                    },
+                    {
+                        "targets": [4],
+                        "visible": false
+                    }
+                ],
+                columns: [
+                    { data : "DisplayName.UserLocalizedLabel.Label"},
+                    { data : "LogicalName" },
+                    { data : "AutoNumberFormat"},
+                    { data : "DisplayName"},
+                    { data : "Description.UserLocalizedLabel.Label" }
+                ]
+            });
+          
+
+            table.on( 'select', function ( e, dt, type, indexes ) {
+                if ( type === 'row' ) {
+                    debugger;
+                    var data = table.rows(indexes).data()[0];
+                    let logicalname = document.getElementById('lnameANP');
+                    let displayname = document.getElementById('dnameANP');
+                    let description = document.getElementById('descANP');
+                    let format = document.getElementById('nformatANP');
+
+                    logicalname.value = data.LogicalName;
+                    displayname.value = data.DisplayName.UserLocalizedLabel.Label;
+                    description.value = data.Description.UserLocalizedLabel.Label;
+                    format.value = data.AutoNumberFormat;
+                }
+            });  
+            //Styling table
+            //   document.getElementById('existingANtable_wrapper').classList.remove('form-inline');
+            $('#existingANtable_wrapper').find('div').first().remove();
+            $('#existingANtable_wrapper').find('div').first().css('width', '100%');
+            document.getElementById('existingANtable').style.width = '100%';          
+        }
+    });
 }
+
+$("#entity-input").change(function () {
+    Anc.LoadExistingAutoNumbers();
+});
+
+// Select table to populate AN Properties
+// $("#table-body tr").click(function() {
+//     var selected = $(this).hasClass("highlight");
+//     $("#table-body tr").removeClass("highlight");
+//     if(!selected)
+//             $(this).addClass("highlight");
+// });
+
+// var table = $('#existingANtable').DataTable();
+
+// $('#existingANtable tbody').on( 'click', 'tr', function () {
+    
+//     if ( $(this).hasClass('highlight') ) {
+//         $(this).removeClass('highlight');
+//     }
+//     else {
+//         $('existingANtable tr.highlight').removeClass('highlight');
+//         $(this).addClass('highlight');
+//     }
+// } );
+
+// $('#button').click( function () {
+//     $('#existingANtable tbody').row('.highlight').remove().draw( false );
+// } );
+
+
 
 let populateProperties = () => {
     console.log("Populated Properties!");
 }
 
-let populateExistingAN = () => {
-    
-}
-
-// let tableRow = document.getElementsByTagName("tr");
-
-document.getElementsByTagName("tr")[1].addEventListener("click", function(){
-    console.log("Clicked");
-});
+// document.getElementsByTagName("tr")[1].addEventListener("click", function(){
+//     console.log("Clicked");
+// });
